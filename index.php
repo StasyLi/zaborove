7ec1bd278a6ce32f59be4dc87b7924645f7652a8
StasyLi <68120957+StasyLi@users.noreply.github.com>
2021-02-03 19:47:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,59 +7,8 @@
 </head>
 <body>
 
-<?php
-
-$link = mysqli_connect("127.0.0.1", "root", "pas4root", "zaborove");
-if ($link==false) {
-print("OH GOD! We've got a problem with link <br>");
-}
-if ($link!=false) {
-print("Everything's alright with link <br>");
-}
-$uploaddir = '/scripts/u-images/';
-$uploadfile = $uploaddir . basename($_FILES['pict']['name']);
-
-define ('SITE_ROOT', realpath(dirname(__FILE__)));
-//move_uploaded_file($_FILES['file']['tmp_name'], SITE_ROOT.'scripts/u-images/');
-
-//move_uploaded_file($_FILES['pict']['tmp_name'],  "$uploadfile") - that was in if construction
-
-
-if (move_uploaded_file($_FILES['pict']['tmp_name'], "$uploaddir")) {
-echo ("The uploading of our admirable file ended successfully!!!!<br>");
-} else {
-echo ("Oh no! :/ THere's an error with uploading.<br>");
-}
-
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$feedback = $_POST['feedback'];
-
-$query = "INSERT INTO feedbacks (Name, Email, Feedback, Photo) VALUES ('{$name}', '{$email}', '{$feedback}', 'address-for-photo');";
-$result = mysqli_query($link, $query);
-
-if ($result == false) {
-print ("There is an error with inserting our perfect data<br>");
-};
-if ($result == true) {
-print ("Yaho! The request was sent<br>");
-}
-
-$req = "SELECT Name, Feedback FROM feedbacks";
-$response = mysqli_query($link, $req);
-while ($row = mysqli_fetch_array($response)){
-print("Name: " . $row['Name'] . ";<br>Feelings: " . $row['Feedback'] . "<br>");
-}
-
-
-//for redirect
-//header('Location: index.html');
-
-?>
-
 <p align="center"><h1>Form for feedbacks</h1>
-<form method="post" name="forma" enctype="multipart/form-data">
+<form action ="send.php" method="post" name="forma" enctype="multipart/form-data">
 
 <fieldset>
 <label for="name">Your name</label>
@@ -83,10 +32,25 @@ print("Name: " . $row['Name'] . ";<br>Feelings: " . $row['Feedback'] . "<br>");
 </fieldset>
 
 <fieldset>
-<input id="button" type="submit" value="Send feedback">
+<input name="knopka" id="button" type="submit" value="Send feedback">
 </fieldset>
 
 </form>
 
+<?php
+
+if (isset($_POST['knopka'])) {
+print ("hi Katia");
+}
+
+$link = mysqli_connect("127.0.0.1", "root", "pas4root", "zaborove");
+$req = "SELECT Name, Feedback FROM feedbacks";
+$response = mysqli_query($link, $req);
+while ($row = mysqli_fetch_array($response)){
+print("Name: " . $row['Name'] . ";<br>Feelings: " . $row['Feedback'] . "<br>");
+}
+
+
+?>
 </body>
 </html>
